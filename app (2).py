166fd4d49249fbb10b5fb531708700f4ccee0b20
tharakdakka2-netizen <tharakdakka2@gmail.com>
2026-02-06 23:19:30 +0530

    Add files via upload

---
+++ app (2).py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "4ba54cb7-0094-45a6-a88d-3dc25471d22f",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
